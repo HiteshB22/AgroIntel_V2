68d7ddc9cda3bde1f2caea688b0aa8b0a07269e5
unknown <HiteshB22>
2026-04-30 19:55:11 +0530

    setup node-server

---
+++ node-server/src/app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import healthRoutes from './routes/health.routes.js';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/health', healthRoutes);
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+export default app ;
